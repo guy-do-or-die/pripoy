42380763dc1d3342a9b0cc71e3ef551a6463ed05
Egor Gusev <guy.do.or.die@gmail.com>
2014-09-20 04:19:44 +0400

    greetings

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('here we are!')
+  response.send('here we are! #pripoy')
 })
 
 app.listen(app.get('port'), function() {
