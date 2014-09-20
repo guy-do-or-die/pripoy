adbe21279d8b50a0faba9e026be1c6b2de8b558e
Egor Gusev <guy.do.or.die@gmail.com>
2014-09-20 18:42:40 +0400

    favicon

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-var express = require('express')
+var express = require('express');
+var path = require('path');
 var app = express();
 
 app.set('port', (process.env.PORT || 3000))
 app.use('/', express.static(__dirname + '/public'))
+app.use(express.favicon(path.join(__dirname, 'public', 'img', 'sharp.ico')));
 
 app.listen(app.get('port'), function() {
   console.log("got it!")
