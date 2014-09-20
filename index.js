e64d1da6c01e9a9a21d8e5c4977ba909875112d5
Egor Gusev <guy.do.or.die@gmail.com>
2014-09-20 18:12:33 +0400

    player is added

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 var express = require('express')
 var app = express();
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
-
-app.get('/', function(request, response) {
-  response.send('here we are! #pripoy\n<h1>BLACK METAL!! /m/ /m/</h1>');
-})
+app.set('port', (process.env.PORT || 3000))
+app.use('/', express.static(__dirname + '/public'))
 
 app.listen(app.get('port'), function() {
   console.log("got it!")
---
+++ public/js/player.js
@@ -0,0 +1,114 @@
+(function(window, $) {
+
+  window.Player = function Player($parent) {
+    var pl = this;
+    var sm = soundManager;
+    var currentSound = null;
+    
+    this.sounds = [] 
+
+    this.body = $('<span style="position:absolute;" class="container"><a class="icon" href="#" ' +  
+      'onclick="event.preventDefault();player.trigger()"><i class="icon-resume"></i></a></span>');
+    
+    var $caption = $('<span style="margin-left:10px; font-size: small; color: grey;"></span>');
+    pl.body.append($caption);
+    $parent.append(pl.body);
+    
+    $(pl.body).draggable({
+      containment: 'window',
+      start: function() {
+        $(this).addClass('sharp-180'); 
+      },
+      stop: function() {
+        $(this).removeClass('sharp-180'); 
+      }
+    });
+    
+    function clearPlayer() {
+      if (pl.sounds.length > 0) {
+        $.each(pl.sounds, function(sound) {
+          currentSound.stop();
+          pl.sounds = [];
+        });
+      }
+    }
+
+    function createSound(url, id) {
+      if (currentSound) {
+        currentSound.stop();
+      }
+      
+      currentSound = sm.createSound({
+        id:'track',
+        url: url,
+        onplay: pl.events.play,
+        onstop: pl.events.stop,
+        onpause: pl.events.pause,
+        onresume: pl.events.resume,
+        onfinish: pl.events.finish,
+        type: 'mp3'
+      });
+     
+      pl.sounds.push(currentSound); 
+      currentSound.play();
+    };
+
+    this.play = function(url) {
+      createSound(url);
+    }; 
+
+    this.pause = function(data) {
+      if (currentSound) {
+        currentSound.pause();
+      }
+    };
+    
+    this.resume = function(data) {
+      if (currentSound) {
+        currentSound.resume();
+      }
+    };
+
+    this.trigger = function() {
+      if (currentSound) {
+        if (currentSound.paused) {
+          pl.resume();
+        } else {
+          pl.pause();
+        }
+      }
+    };
+
+    this.caption = function(text) {
+      if (text) {
+        $caption.html(text);
+      }
+    };
+
+    this.events = {
+
+      play: function() {
+        $(pl.body).find('i').removeClass('icon-play').addClass('icon-pause'); 
+      },
+
+      stop: function() {
+        $(pl.body).find('i').removeClass('icon-pause').removeClass('icon-resume'); 
+      },
+
+      pause: function() {
+        $(pl.body).find('i').removeClass('icon-pause').addClass('icon-play');
+      },
+
+      resume: function() {
+        $(pl.body).find('i').removeClass('icon-play').addClass('icon-pause');
+      },
+
+      finish: function() {
+        $(pl.body).find('i').removeClass('icon-pause').addClass('icon-play'); 
+      }
+
+    }
+
+  }
+
+})(window, jQuery)
