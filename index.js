aae8685acc6f1f0fb7cce3879ea507037a6fa82d
Egor Gusev <guy.do.or.die@gmail.com>
2014-09-21 17:09:46 +0400

    favicon polishing

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var app = express();
 
 app.set('port', (process.env.PORT || 3000))
 app.use('/', express.static(__dirname + '/public'))
-app.use(express.favicon(path.join(__dirname, 'public', 'img', 'sharp.ico')));
+app.use(express.favicon(path.join(__dirname, 'public', 'img', 'favicon.ico')));
 
 app.listen(app.get('port'), function() {
   console.log("got it!")
